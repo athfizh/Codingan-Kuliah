2f991289838fb968743198449216d65c9a7b53f4
athfizh <athaullahafizh@gmail.com>
2024-09-11 13:25:18 +0700

    Codingan Kuliah

---
+++ daspro-jobsheet3/ContohVariabel05.java
@@ -0,0 +1,15 @@
+public class ContohVariabel05 {
+
+    public static void main(String[] args) {
+        String Hobisayaadalah = "Hobi saya adalah Berlari";
+        boolean isPandai = true;
+        char jenisKelamin = 'L';
+        byte _umurSayaSekarang = 18;
+        double $ipk = 3.75, tinggi = 1.80;
+        System.out.println(Hobisayaadalah);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+    }
+}
